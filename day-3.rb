d4615f13d5ebbda720c7be27db87ed10d0384825
mn <marvoosh@gmail.com>
2021-12-09 09:23:05 +0000

    day 3 complete

--- day-3.rb
+++ day-3.rb
@@ -22,4 +22,57 @@ end
 #  p final_binary
 gamma = 3808
 epsi = 287
-p gamma * epsi
+# p gamma * epsi
+
+
+def common(input)
+  i = 0
+  common = input
+  while i < input[0].length do
+    input = common
+    common = []
+    count_0 = 0
+    count_1 = 0
+    input.each do |binary|
+      binary[i] == "0" ? count_0 += 1 : count_1 += 1
+    end
+    input.each do |binary|
+      if count_0 > count_1
+        common << binary if binary[i] == "0"
+      else
+        common << binary if binary[i] == "1"
+      end
+    end
+    return common if common.length == 1
+    i += 1
+  end
+end
+
+def not_common(input)
+  i = 0
+  not_common = input
+  while i < input[0].length do
+    input = not_common
+    not_common = []
+    count_0 = 0
+    count_1 = 0
+    input.each do |binary|
+      binary[i] == "0" ? count_0 += 1 : count_1 += 1
+    end
+    input.each do |binary|
+      if count_0 < count_1 || count_0 == count_1
+        not_common << binary if binary[i] == "0"
+      else
+        not_common << binary if binary[i] == "1"
+      end
+    end
+    return not_common if not_common.length == 1
+    i += 1
+  end
+end
+
+p common(clean_input)
+p not_common(clean_input)
+oxygen = 3443
+co2 = 1357
+p oxygen * co2
